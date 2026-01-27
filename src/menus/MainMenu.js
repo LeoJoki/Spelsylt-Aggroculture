b749012e1724810a94899213f096fbc74e84c7c1
leo <leo.jokijarvi@elev.ga.ntig.se>
2026-01-27 14:54:43 +0100

    Skapade en tutorial menu och ändrade på dashen

--- src/menus/MainMenu.js
+++ src/menus/MainMenu.js
@@ -1,5 +1,6 @@
 import Menu from './Menu.js'
 import CreditsMenu from './CreditsMenu.js'
+import TutorialMenu from './TutorialMenu.js'
 
 export default class MainMenu extends Menu {
     getTitle() {
@@ -23,6 +24,14 @@ export default class MainMenu extends Menu {
                 action: () => {
                     this.game.gameState = 'CREDITS'
                     this.game.currentMenu = new CreditsMenu(this.game)
+                }
+            },
+            {
+                text: 'Tutorial',
+                key: 'v',
+                action: () => {
+                    this.game.gameState = 'TUTORIAL'
+                    this.game.currentMenu = new TutorialMenu(this.game)
                 }
             }
         ]
