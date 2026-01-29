5eea8ec987d744bc4a3356c28d1ad60060b28b11
leo <leo.jokijarvi@elev.ga.ntig.se>
2026-01-29 13:16:29 +0100

    test

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   build: {
-    assetsInlineLimit: 0, // Disable inlining assets as base64
     base: '/Spelsylt-Aggroculture/', // Ändra detta till ditt repo-namn
   }
 })
