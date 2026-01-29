9ac4c2b3dcacfa065056ffca2f54688966aa80ec
leo <leo.jokijarvi@elev.ga.ntig.se>
2026-01-29 13:26:33 +0100

    trailing slash

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   build: {
-    base: '/Spelsylt-Aggroculture/', // Ändra detta till ditt repo-namn
+    base: '/Spelsylt-Aggroculture', // Ändra detta till ditt repo-namn
   }
 })
